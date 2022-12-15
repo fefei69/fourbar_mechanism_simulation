986c61c3f9d8f85ae3c3fed792c936b18f5e1ba6
fefei69 <coolpixs04@gmail.com>
2022-12-15 17:05:14 +0800

    first commit

---
+++ 平面機構模擬1/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace 平面機構模擬_fourbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisX2.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea1.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisY2.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.BackColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 45);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 10;
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 10;
+            series2.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.Black;
+            series3.Legend = "Legend1";
+            series3.MarkerSize = 10;
+            series3.Name = "Series3";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.Black;
+            series4.Legend = "Legend1";
+            series4.MarkerColor = System.Drawing.Color.Black;
+            series4.MarkerSize = 10;
+            series4.Name = "Series4";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Legend = "Legend1";
+            series5.MarkerColor = System.Drawing.Color.Black;
+            series5.MarkerSize = 10;
+            series5.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Diamond;
+            series5.Name = "Series5";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.MarkerColor = System.Drawing.Color.Black;
+            series6.MarkerSize = 10;
+            series6.Name = "Series6";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series7.Legend = "Legend1";
+            series7.MarkerImage = "C:\\Users\\User\\Downloads\\地2.png";
+            series7.MarkerSize = 1;
+            series7.Name = "Series7";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(600, 600);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "clock";
+            title1.BackColor = System.Drawing.Color.White;
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Four-Bar Simulation sol1";
+            title1.Text = "Four-Bar Simulation sol1";
+            title1.TextStyle = System.Windows.Forms.DataVisualization.Charting.TextStyle.Shadow;
+            this.chart1.Titles.Add(title1);
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 598);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clockwise";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 598);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 27);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Counterclockwise";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(618, 45);
+            this.chart2.Name = "chart2";
+            series8.BorderColor = System.Drawing.Color.Transparent;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Legend = "Legend1";
+            series8.MarkerSize = 10;
+            series8.Name = "Series1";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Legend = "Legend1";
+            series9.MarkerSize = 10;
+            series9.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series9.Name = "Series2";
+            series10.BorderColor = System.Drawing.Color.Black;
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series10.Color = System.Drawing.Color.Black;
+            series10.Legend = "Legend1";
+            series10.MarkerSize = 10;
+            series10.Name = "Series3";
+            series11.BorderColor = System.Drawing.Color.Black;
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series11.Color = System.Drawing.Color.Black;
+            series11.Legend = "Legend1";
+            series11.MarkerSize = 10;
+            series11.Name = "Series4";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series12.Legend = "Legend1";
+            series12.MarkerColor = System.Drawing.Color.Black;
+            series12.MarkerSize = 10;
+            series12.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Diamond;
+            series12.Name = "Series5";
+            series13.BorderColor = System.Drawing.Color.Transparent;
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series13.Legend = "Legend1";
+            series13.MarkerSize = 10;
+            series13.Name = "Series6";
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series14.Legend = "Legend1";
+            series14.MarkerImage = "C:\\Users\\User\\Downloads\\地2.png";
+            series14.Name = "Series7";
+            this.chart2.Series.Add(series8);
+            this.chart2.Series.Add(series9);
+            this.chart2.Series.Add(series10);
+            this.chart2.Series.Add(series11);
+            this.chart2.Series.Add(series12);
+            this.chart2.Series.Add(series13);
+            this.chart2.Series.Add(series14);
+            this.chart2.Size = new System.Drawing.Size(600, 600);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title2.Name = "Four-Bar Simulation sol2";
+            title2.Text = "Four-Bar Simulation sol2";
+            title2.TextStyle = System.Windows.Forms.DataVisualization.Charting.TextStyle.Shadow;
+            this.chart2.Titles.Add(title2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1443, 776);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ 平面機構模擬1/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 平面機構模擬_fourbar
+{
+    public partial class Form1 : Form
+    {
+        double x;
+        int R = 50;
+        //請輸入固定軸樞座標
+        int[] M = { 1, 1 };
+        int[] Q = { 5, 2 };
+        double A_X, A_Y, A_R , Q_X , Q_Y , Q_R;
+        //intialize input angle theta2
+        double theta2 = 0;
+        double MQ;
+        int dir = 5;
+        
+        
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        double MA =2, AB=3, QB=4; 
+      
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.ChartAreas[0].AxisX.Minimum = -10;
+            chart1.ChartAreas[0].AxisX.Maximum = 10;
+            chart1.ChartAreas[0].AxisY.Minimum = -10;
+            chart1.ChartAreas[0].AxisY.Maximum = 10;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chart2.ChartAreas[0].AxisX.Minimum = -10;
+            chart2.ChartAreas[0].AxisX.Maximum = 10;
+            chart2.ChartAreas[0].AxisY.Minimum = -10;
+            chart2.ChartAreas[0].AxisY.Maximum = 10;
+            chart2.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart2.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+        }
+
+        private void Form1_load(object sender, EventArgs e)
+        {
+       
+            timer1.Tick += timer1_Tick_1;
+            timer1.Interval = 10;
+
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+            
+        {
+            MQ = Math.Sqrt((M[0] - Q[0]) ^ 2 - (M[1] - Q[1]) ^ 2);
+            //請輸入桿長
+            double[] anArray = { MQ, MA, AB, QB };
+            double MaxLinkage = anArray.Max();
+            double MinLinkage = anArray.Min();
+            double sum = anArray.Sum();
+            double D_linkage = (MaxLinkage + MinLinkage) * 2 - sum;
+            double theta1 = Math.Atan2(M[1] - Q[1], M[0] - Q[0]) * (180 / Math.PI); //in degrees
+            //要動的角度 theta2 用徑度表示(radian)
+            double theta3 = 0;
+            double theta4 = 0;
+            A_X = M[0] + MA * Math.Cos(theta2);
+            A_Y = M[1] + MA * Math.Sin(theta2);
+            A_R = AB;
+            Q_X = Q[0];
+            Q_Y = Q[1];
+            Q_R = QB;
+            //L M N A B C
+            double L = Math.Pow(A_X, 2) + Math.Pow(A_Y, 2) + Math.Pow(Q_X, 2) + Math.Pow(Q_Y, 2) - Math.Pow(A_R, 2) + Math.Pow(Q_R, 2) - 2 * (A_X * Q_X + A_Y * Q_Y);
+            double m = 2 * Q_R * (A_X - Q_X);
+            double N = 2 * Q_R * (A_Y - Q_Y);
+            double A = L + m;
+            double B = -2 * N;
+            double C = L - m;
+            //判別式解A B C -> D = B^2 - 4AC 
+            double D = Math.Pow(B, 2) - 4 * A * C;
+            double x1, x2;
+            x1 = 0;
+            x2 = 0;
+            if (D == 0)
+            {
+                x1 = -C / B;
+                x2 = 1000000; //x2 -> infinite
+                
+            }
+            else if (D > 0)
+            {
+                x1 = (-B + Math.Sqrt(D)) / (2 * A);
+                x2 = (-B - Math.Sqrt(D)) / (2 * A);
+            }
+            else
+            {
+                Console.WriteLine("D < 0 ,no solution");
+            }
+            Console.WriteLine("x1");
+            Console.WriteLine(x1);
+            Console.WriteLine("x2");
+            Console.WriteLine(x2);
+            //Console.WriteLine("x1:",x1,x2);
+            double beta1 = 2 * Math.Atan(x1); //in radian
+            double beta2 = 2 * Math.Atan(x2); //in radian
+
+            //兩圓交點方程式
+            double xc1 = Q_X + Q_R * Math.Cos(beta1);
+            double xc2 = Q_X + Q_R * Math.Cos(beta2);
+            double yc1 = Q_Y + Q_R * Math.Sin(beta1);
+            double yc2 = Q_Y + Q_R * Math.Sin(beta2);
+            double theta3_1 = (xc1 - A_X) / (yc1 - A_Y);
+            double theta3_2 = (xc2 - A_X) / (yc2 - A_Y);
+            double theta4_1 = beta1;
+            double theta4_2 = beta2;
+
+            if (chart1.Series[0].Points.Count > 1)
+            {
+               chart1.Series[0].Points.RemoveAt(0);
+               chart1.Series[1].Points.RemoveAt(0);
+               chart1.Series[5].Points.RemoveAt(0);
+
+               chart2.Series[0].Points.RemoveAt(0);
+               chart2.Series[1].Points.RemoveAt(0);
+               chart2.Series[5].Points.RemoveAt(0);
+
+            }
+            //chart1
+            //函數 x 為角度 
+            chart1.Series[0].Points.AddXY(M[0],M[1]);
+            chart1.Series[0].Points.AddXY(A_X, A_Y);
+            chart1.Series[1].Points.AddXY(A_X, A_Y);
+            chart1.Series[1].Points.AddXY(xc1, yc1);
+            chart1.Series[5].Points.AddXY(xc1, yc1);
+            chart1.Series[5].Points.AddXY(Q_X, Q_Y);
+            
+            //x axis y axis
+            chart1.Series[2].Points.AddXY(-10, 0);
+            chart1.Series[2].Points.AddXY(10, 0);
+            chart1.Series[3].Points.AddXY(0, 10);
+            chart1.Series[3].Points.AddXY(0, -10);
+            //箭頭
+            chart1.Series[4].Points.AddXY(0, 10);
+            chart1.Series[4].Points.AddXY(10, 0);
+            //接地符號
+            chart1.Series[6].Points.AddXY(Q[0], Q[1]);
+            chart1.Series[6].Points.AddXY(M[0], M[1]);
+            //chart2
+            //函數 x 為角度 
+            chart2.Series[0].Points.AddXY(M[0], M[1]);
+            chart2.Series[0].Points.AddXY(A_X, A_Y);
+            chart2.Series[1].Points.AddXY(A_X, A_Y);
+            chart2.Series[1].Points.AddXY(xc2, yc2);
+            chart2.Series[5].Points.AddXY(xc2, yc2);
+            chart2.Series[5].Points.AddXY(Q_X, Q_Y);
+
+            //x axis y axis
+            chart2.Series[2].Points.AddXY(-10, 0);
+            chart2.Series[2].Points.AddXY(10, 0);
+            chart2.Series[3].Points.AddXY(0, 10);
+            chart2.Series[3].Points.AddXY(0, -10);
+            //箭頭
+            chart2.Series[4].Points.AddXY(0, 10);
+            chart2.Series[4].Points.AddXY(10, 0);
+            //接地符號
+            chart2.Series[6].Points.AddXY(Q[0], Q[1]);
+            chart2.Series[6].Points.AddXY(M[0], M[1]);
+            if (dir == 1)
+            {
+                theta2 -= 1;
+            }
+            else
+            {
+                theta2 += 1;
+            }
+            //Console.WriteLine(dir);
+
+
+            if (chart1.Series[0].Points.Count > 0)
+            {
+                chart1.Series[0].Points.RemoveAt(0);
+                chart1.Series[1].Points.RemoveAt(0);
+                chart1.Series[5].Points.RemoveAt(0);
+
+                chart2.Series[0].Points.RemoveAt(0);
+                chart2.Series[1].Points.RemoveAt(0);
+                chart2.Series[5].Points.RemoveAt(0);
+
+            }
+
+            //設定x軸y軸大小
+            /*chart1.ChartAreas[0].AxisX.ScaleView.Position = 100;
+            chart1.ChartAreas[0].AxisY.ScaleView.Position = 100;
+            chart1.ChartAreas[0].AxisX.Minimum = -100;
+            chart1.ChartAreas[0].AxisX.Maximum = 100;
+            chart1.ChartAreas[0].AxisY.Minimum = -100;
+            chart1.ChartAreas[0].AxisY.Maximum = 100;
+            */
+
+
+        
+            //chart1.ChartAreas[0].AxisX.Minimum = chart1.Series[0].Points[0].XValue;
+            //chart1.ChartAreas[0].AxisX.Maximum = x;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dir = 1;//順時鐘轉
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dir = -1;//逆時鐘轉
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+
+            }
+        }
+        
+
+
+        }
+        
+
+
+    
+}
